79c6ae8dd55755ba97edd99fdab4815016412bc0
Hien Tran <voss.pro@gmail.com>
2020-10-15 16:31:57 +0700

    update

--- signaling/NodeJS/app.js
+++ signaling/NodeJS/app.js
@@ -1,6 +1,13 @@
 const WebSocket = require('ws');
+const fs = require('fs');
+const https = require('https');
 
-const wss = new WebSocket.Server({ port: 8080 }, () => {
+const server = https.createServer({
+    key: fs.readFileSync("../privkey.pem"),
+    cert: fs.readFileSync("../fullchain.pem")
+});
+
+const wss = new WebSocket.Server({ server }, () => {
     console.log("Signaling server is now listening on port 8080")
 });
 
@@ -15,7 +22,7 @@ wss.broadcast = (ws, data) => {
 
 wss.on('connection', (ws) => {
     console.log(`Client connected. Total connected clients: ${wss.clients.size}`)
-    
+
     ws.onmessage = (message) => {
         console.log(message.data + "\n");
         wss.broadcast(ws, message.data);
